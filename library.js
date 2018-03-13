a483d1f90f81ba30df1a95a5bccd7e2756812763
Jacob Grzetich <grzetich20@gmail.com>
2018-03-12 21:17:42 -0700

    change classes

--- library.js
+++ library.js
@@ -7,10 +7,24 @@ function book(title , publicationDate , callNumber, author){
   this.title = title;
   this.available = true;
   this.publicationDate = new Date(publicationDate);
-  this.checkOutDate = new Date();
+  this.checkOutDate;
   this.callNumber = callNumber;
   this.authors = [];
-
   //Pushes authors into authors array
   this.authors.push(author);
+
+  this.checkOut = function(currentDate){
+    let date = currentDate;
+    this.checkOutDate;
+    this.available = false;
+  }
+}
+
+function patreon(firstName, lastName, libraryCardNumber){
+  this.firsName = firstName;
+  this.lastName = lastName;
+  this.libraryCardNumber = libraryCardNumber;
+  this.booksCheckedOut = [];
+  this.fines;
+  //Add Book To Array
 }
