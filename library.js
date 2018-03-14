447384c71f1647265cf90b1b52d018cbd6268581
Jacob Grzetich <grzetich20@gmail.com>
2018-03-13 20:00:41 -0700

    added books with books in it also changed the books class

--- library.js
+++ library.js
@@ -8,15 +8,29 @@ function book(title , publicationDate , callNumber, author){
   this.available = true;
   this.publicationDate = new Date(publicationDate);
   this.checkOutDate;
+  this.fine = 0;
   this.callNumber = callNumber;
   this.authors = [];
   //Pushes authors into authors array
   this.authors.push(author);
 
+  //sets current date to the checkedoutDate and sets available to false
   this.checkOut = function(currentDate){
     let date = currentDate;
     this.checkOutDate;
     this.available = false;
+  }
+
+  //set the avaiablity to true
+  this.checkIn = function(){
+    this.available = true;
+  }
+
+  //Is overDue function needs work
+  this.isOverdue = function(currentDate){
+    if((currentDate - this.checkOutDate) > 14){
+      this.fine += 5.00;
+    }
   }
 }
 
@@ -24,7 +38,16 @@ function patreon(firstName, lastName, libraryCardNumber){
   this.firsName = firstName;
   this.lastName = lastName;
   this.libraryCardNumber = libraryCardNumber;
-  this.booksCheckedOut = [];
+  this.bookBackpack = [];
   this.fines;
-  //Add Book To Array
+
+  //Adds Book To Array
+  this.addBookToBackpack = function(book){
+    this.bookBackpack.push(book);
+  }
+
+  //retrun Book from bookBackpack
+  this.removeBookFromBackpack = function(callNumber){
+
+  }
 }
